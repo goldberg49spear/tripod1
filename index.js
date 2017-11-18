4e42b5a47019ab2c04ce13ae1044131c18d074e0
goldberg49spear <mathew.adithya94@gmail.com>
2017-11-18 22:09:23 +0530

    new change

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-$(function(){
-    var treeView = $("#treeview").dxTreeView({ 
-        items: products,
-        width: 500,
-        searchEnabled: true
-    }).dxTreeView("instance");
+var express = require('express');
+var bodyParser = require('body-parser');
+var pg = require('pg');
 
-    $("#searchMode").dxSelectBox({
-        dataSource: ["contains", "startswith"],
-        value: "contains",
-        onValueChanged: function(data) {
-            treeView.option("searchMode", data.value);
-        }
+var app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.get('/update', function(req, res) {
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        // watch for any connect issues
+        if (err)  
+		{console.log(err);
+			 res.status(400).json({error: err.message});}
+        
+                
+                else {
+					alert("working");
+					conn.query(
+            'SELECT firstName,LastName,Phone,MobilePhone from salesforce14.Contact'            
+                  );
+					
+					
+                    done();
+                    res.json(result);
+                }
+            }
+        );
     });
+
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
