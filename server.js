d80a47b19be8d9f71071eed08682fba66d4fd618
goldberg49spear <mathew.adithya94@gmail.com>
2017-11-19 12:48:38 +0530

    changed sever.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/db/dropTable', function(req,res){
 
 app.set('port', process.env.PORT || 3001);
 
-app.use(express.static(__dirname + '/client')); 
+app.use(express.static(__dirname + '/dist')); 
 app.use(errorHandler());
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
