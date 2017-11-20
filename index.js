fdf741204ecb9005fb0d1cfae21ec8634149f344
goldberg49spear <33773100+goldberg49spear@users.noreply.github.com>
2017-11-20 10:53:22 +0530

    adding render samplehtml.html

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ var app = express();
 
 app.set('port', process.env.PORT || 5000);
 
-app.use(express.static(__dirname +'/public'));
+app.use(express.static());
 app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+  response.render('samplehtml.html')
+});
 
 app.get('/update', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
