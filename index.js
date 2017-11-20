487e0912934d864b47e868481f1643d72489b49b
goldberg49spear <33773100+goldberg49spear@users.noreply.github.com>
2017-11-20 14:32:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,14 @@ app.use(express.static(__dirname +'/samplehtml.html'));
 
 
 app.use(bodyParser.json());
+app.set('view engine', 'ejs');
 
 app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
 app.get('/', function(request, response) {
     //var result = 'App is running'
-    response.sendFile(__dirname+'/samplehtml.html');
+    response.render(__dirname+'/samplehtml.html');
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
